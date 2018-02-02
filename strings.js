97e34d02e4dd532c1fe9081c77251e5c0752b88d
Henry <henry.cooke@bbc.co.uk>
2018-02-02 12:11:10 +0100

    first commit

---
+++ strings.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let EnglishStrings = {
+    //"WELCOME_MSG": "Welcome to {{ skillName }}",
+    "WELCOME_MSG": ".",
+    "HELP_MSG": "Welcome to {{ skillName }}. You can play, stop, resume listening.  How can I help you ?",
+    "UNHANDLED_MSG" : "Sorry, I could not understand what you've just said.",
+    "RESUME_MSG" : "Resuming {{ skillName }}",
+    "NOT_POSSIBLE_MSG" : "Sorry, that's not possible. You can ask me to stop or pause to stop listening.",
+    "STOP_MSG" : "Goodbye."
+};
+module.exports = {
+    "en-GB": {
+        "translation": EnglishStrings
+    }
+};
